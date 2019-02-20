f12cffdf6c44f4ebb2c02d624587c0fce243a8f0
Funky <708863861@qq.com>
2019-02-20 15:10:06 +0800

    first commit

---
+++ lib/core.py
@@ -0,0 +1,43 @@
+import docker
+import settings
+from aliyunsdkcore import client
+from aliyunsdkcr.request.v20160607 import GetImageLayerRequest
+
+def Check(ImageName):
+    client.region_provider.add_endpoint(settings.Product_name,
+                                        settings.Region_id,
+                                        settings.End_point)
+    namespace, nametag = ImageName.split('/')
+    image, tag = nametag.split(':')
+    request = GetImageLayerRequest.GetImageLayerRequest()
+    request.set_RepoName(image)
+    request.set_RepoNamespace(settings.RepoNamespace)
+    request.set_Tag(tag)
+    response = eval(client.AcsClient(settings.id, settings.key, 'cn-hangzhou').do_action_with_exception(request).
+                    decode('utf-8'))
+    if response['data']['image'] == {}:
+        print("This tag does not exist.")
+        return 1, ""
+    else:
+        print("Mirror Image is: "+settings.RepoUrl+"/"+settings.RepoNamespace+"/"+image+":"+tag)
+        return 0, settings.RepoUrl+"/"+settings.RepoNamespace+"/"+image+":"+tag
+
+
+def Sync(ImageName):
+    namespace, nametag = ImageName.split('/')
+    imagename, tag = nametag.split(':')
+    docker_client = docker.from_env()
+    try:
+        docker_client.api.login(username=settings.username, password=settings.password, registry=settings.RepoUrl)
+        image = docker_client.images.pull(ImageName)
+        image.tag(repository=settings.RepoUrl+"/"+settings.RepoNamespace+"/"+imagename, tag=tag)
+        docker_client.images.push(repository=settings.RepoUrl+"/"+settings.RepoNamespace+"/"+imagename, tag=tag)
+        docker_client.images.remove(settings.RepoUrl+"/"+settings.RepoNamespace+"/"+imagename+":"+tag)
+        return 0, settings.RepoUrl+"/"+settings.RepoNamespace+"/"+imagename+":"+tag
+    except:
+        print("Sync Error!")
+        return 1, ""
+
+
+if __name__ == '__main__':
+    Check(ImageName="k8s.gcr.io/kube-proxy-amd64:v1.11.0")
---
+++ pull.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import docker
+import logging
+import settings
+import optparse
+import grpc
+import grpc_lib.image_pull_pb2 as image_pull_pb2
+import grpc_lib.image_pull_pb2_grpc as image_pull_pb2_grpc
+
+
+def run(host, image):
+    with grpc.insecure_channel(host+':50051') as channel:
+        stub = image_pull_pb2_grpc.ImagePullStub(channel)
+        response = stub.CheckImageExists(image_pull_pb2.ImageName(Image=image))
+        if response.stats == 0:
+            client = docker.client.from_env()
+            client.images.pull(response.ImageMirrorName)
+        else:
+            response = stub.PullImage(image_pull_pb2.ImageName(Image=image))
+            if response.stats == 0:
+                client = docker.client.from_env()
+                client.images.pull(response.ImageMirrorName)
+            else:
+                print("This image does not exist.")
+
+
+def main():
+    parser = optparse.OptionParser(
+        'usage: pull.py -i <imagename> -h <mirror server>'
+    )
+    parser.add_option('-i', dest='image', type='string', help='specify images')
+    parser.add_option('-h', dest='host', type='string', help='specify host')
+    (options, args) = parser.parse_args()
+    if options.host == "":
+        run(settings.host, options.image)
+    else:
+        run(options.host, options.image)
+
