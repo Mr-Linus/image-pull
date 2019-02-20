dc0075806630b2387f05a62a6a322c3b2c085d1c
Funky <708863861@qq.com>
2019-02-20 17:01:32 +0800

    first commit

--- lib/core.py
+++ lib/core.py
@@ -1,7 +1,9 @@
 import docker
 import settings
 from aliyunsdkcore import client
-from aliyunsdkcr.request.v20160607 import GetImageLayerRequest
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkcr.request.v20160607 import GetImageLayerRequest,CreateRepoRequest
+
 
 def Check(ImageName):
     client.region_provider.add_endpoint(settings.Product_name,
@@ -9,18 +11,25 @@ def Check(ImageName):
                                         settings.End_point)
     namespace, nametag = ImageName.split('/')
     image, tag = nametag.split(':')
-    request = GetImageLayerRequest.GetImageLayerRequest()
-    request.set_RepoName(image)
-    request.set_RepoNamespace(settings.RepoNamespace)
-    request.set_Tag(tag)
-    response = eval(client.AcsClient(settings.id, settings.key, 'cn-hangzhou').do_action_with_exception(request).
+    try:
+        request = GetImageLayerRequest.GetImageLayerRequest()
+        request.set_RepoName(image)
+        request.set_RepoNamespace(settings.RepoNamespace)
+        request.set_Tag(tag)
+        response = eval(client.AcsClient(settings.id, settings.key, 'cn-hangzhou').do_action_with_exception(request).
                     decode('utf-8'))
-    if response['data']['image'] == {}:
-        print("This tag does not exist,Trying to call sync it.")
-        return 1, ""
-    else:
-        print("Mirror Image Exist: "+settings.RepoUrl+"/"+settings.RepoNamespace+"/"+image+":"+tag)
-        return 0, settings.RepoUrl+"/"+settings.RepoNamespace+"/"+image+":"+tag
+        if response['data']['image'] == {}:
+            print("This tag does not exist,Trying to call sync it.")
+            return 1, ""
+        else:
+            print("Mirror Image Exist: " + settings.RepoUrl + "/" + settings.RepoNamespace + "/" + image + ":" + tag)
+            return 0, settings.RepoUrl + "/" + settings.RepoNamespace + "/" + image + ":" + tag
+    except ServerException:
+        print("The image repo does not exists,trying to create it.")
+        request = CreateRepoRequest.CreateRepoRequest()
+        request.add_path_param("RepoName", image+"/")
+        response = eval(client.AcsClient(settings.id, settings.key, 'cn-hangzhou').do_action_with_exception(request).
+                        decode('utf-8'))
 
 
 def Sync(ImageName):
