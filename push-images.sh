708b4593e1a1390fd5206ba46ad4460b8c5f70a4
Funky <708863861@qq.com>
2019-02-24 14:02:34 +0800

    add dockerfile func

--- push-images.sh
+++ push-images.sh
@@ -2,7 +2,7 @@
 
 echo "Build Images..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
-sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-1.12.5 .
+sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-1.13.3 .
 echo "Push Images..."
-sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-1.12.5
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-1.13.3
 sudo docker logout
