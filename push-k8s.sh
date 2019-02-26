d32cc3acb5aa55575a91142141e72390881f7775
Funky <708863861@qq.com>
2019-02-26 23:02:14 +0800

    add update k8s image func

--- push-k8s.sh
+++ push-k8s.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-swapoff -a
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+sudo swapoff -a
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
-kubeadm config images list > image.txt
-docker run --rm -it \
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo kubeadm config images list > image.txt
+sudo docker run --rm -it \
         -v $(pwd)/image.txt:/image-pull/image.txt \
         -v /var/run/docker.sock:/var/run/docker.sock  \
         registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:latest
