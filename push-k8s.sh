d9ba6f30e1c2129311c8ebef777c111081a9d310
Funky <708863861@qq.com>
2019-02-26 22:55:16 +0800

    add update k8s image func

--- push-k8s.sh
+++ push-k8s.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 swapoff -a
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install kubeadm
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 kubeadm config images list > image.txt
 docker run --rm -it \
         -v $(pwd)/image.txt:/image-pull/image.txt \
