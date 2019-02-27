edc85cbc92deeb02359c3c89d7f52ba5eda2a090
Funky <708863861@qq.com>
2019-02-27 14:01:00 +0800

    add update k8s image func

--- push-k8s.sh
+++ push-k8s.sh
@@ -2,7 +2,7 @@
 sudo swapoff -a
 echo "Update System.."
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-echo "Install kubeadm.."
+echo "Install kubeadm & push images.."
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
@@ -11,3 +11,12 @@ sudo docker run --rm -it \
         -v $(pwd)/image.txt:/image-pull/image.txt \
         -v /var/run/docker.sock:/var/run/docker.sock  \
         registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:latest
+echo "Deploy the latest version:"
+version=$(kubeadm config images list | head -1 | awk -F: '{ print $2 }')
+sudo kubeadm config images list > image-k8s.txt
+echo "Build Images..."
+sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
+sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-$version .
+echo "Push Images..."
+sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-$version
+sudo docker logout
