b569da4aaef66b45cae7a1d3a20df5bcc6a07bd9
Funky <708863861@qq.com>
2019-06-28 12:36:57 +0800

    Fix v1.14.2 bugs

--- push-k8s.sh
+++ push-k8s.sh
@@ -2,13 +2,10 @@
 sudo swapoff -a
 echo "Update System.."
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-version=v1.14.3
-vv=1.14.3-00
 echo "Install kubeadm & push images.."
 sudo apt-get update
-sudo apt-get install -y kubelet=$vv kubeadm=$vv kubectl=$vv
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-kubeadm config images list
 sudo kubeadm config images list > image.txt
 sudo docker run --rm -it \
         -v $(pwd)/image.txt:/image-pull/image.txt \
@@ -16,8 +13,8 @@ sudo docker run --rm -it \
         registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:latest
 
 #version=$(kubeadm config images list | head -1 | awk -F: '{ print $2 }')
+version=$(kubeadm config images list | head -1 | awk -F: '{ print $2 }')
 echo "Deploy the latest version: $version"
-sudo kubeadm config images list > image-k8s.txt
 echo "Build Images $version..."
 sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:k8s-$version .
