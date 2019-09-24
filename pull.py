ac660b2a58aa8d408bdb4a56aacc8da0c4d5353f
Funky <mrfunky001@gmail.com>
2019-09-24 14:08:47 +0800

    fix travis error

--- pull.py
+++ pull.py
@@ -8,11 +8,13 @@ import grpc_lib.image_pull_pb2_grpc as image_pull_pb2_grpc
 
 
 def run(host, image_name):
-    with grpc.insecure_channel(host+':50051') as channel:
+    with grpc.insecure_channel(host + ':50051') as channel:
         stub = image_pull_pb2_grpc.ImagePullStub(channel)
-        response = stub.CheckImageExists(image_pull_pb2.ImageName(Image=image_name))
+        response = stub.CheckImageExists(
+            image_pull_pb2.ImageName(
+                Image=image_name))
         if response.stats == 0:
-            print("Mirror image is:"+response.ImageMirrorName)
+            print("Mirror image is:" + response.ImageMirrorName)
             client = docker.client.from_env()
             print("Pulling images...")
             image_object = client.images.pull(response.ImageMirrorName)
@@ -20,7 +22,7 @@ def run(host, image_name):
             print("Changing the image's tag...")
             try:
                 image_object.tag(repository=repo, tag=tag)
-            except:
+            except BaseException:
                 print("Change Tag error!The Image Name maybe exist.")
             client.images.remove(response.ImageMirrorName)
             print("Done.")
@@ -28,9 +30,13 @@ def run(host, image_name):
             print("The image does not exist in the repository, \n" +
                   "try to pull from the original Repo: \n" +
                   image_name)
-            response = stub.PullImage(image_pull_pb2.ImageName(Image=image_name))
+            response = stub.PullImage(
+                image_pull_pb2.ImageName(
+                    Image=image_name))
             if response.stats == 0:
-                print("Sync Finished,Mirror image is:" + response.ImageMirrorName)
+                print(
+                    "Sync Finished,Mirror image is:" +
+                    response.ImageMirrorName)
                 client = docker.client.from_env()
                 print("Pulling images...")
                 image_object = client.images.pull(response.ImageMirrorName)
@@ -38,7 +44,7 @@ def run(host, image_name):
                 repo, tag = image_name.split(":")
                 try:
                     image_object.tag(repository=repo, tag=tag)
-                except:
+                except BaseException:
                     print("Change Tag error!The Image Name maybe exist.")
                 client.images.remove(response.ImageMirrorName)
                 print("Done.")
@@ -52,7 +58,11 @@ def main():
     )
     parser.add_option('-i', dest='image', type="string", help='specify images')
     parser.add_option('-s', dest='host', type="string", help='specify host')
-    parser.add_option('-f', dest='filename', metavar="FILE", help='specify image-name file')
+    parser.add_option(
+        '-f',
+        dest='filename',
+        metavar="FILE",
+        help='specify image-name file')
     (options, args) = parser.parse_args()
     if options.host is None:
         host = settings.host
@@ -63,11 +73,14 @@ def main():
             with open(options.filename) as f:
                 line = f.readline().replace('\n', '')
                 while line:
-                    print("Get image:"+line)
+                    print("Get image:" + line)
                     run(host, str(line))
                     line = f.readline().replace('\n', '')
-        except:
-            print("Read the file:"+options.filename+" Error! or Docker is not running!")
+        except BaseException:
+            print(
+                "Read the file:" +
+                options.filename +
+                " Error! or Docker is not running!")
     elif options.image is not None:
         if (options.image is " ") and (options.filename is not None):
             try:
@@ -77,8 +90,11 @@ def main():
                         print("Get image:" + line)
                         run(host, str(line))
                         line = f.readline().replace('\n', '')
-            except:
-                print("Read the file:" + options.filename + " Error! or Docker is not running!")
+            except BaseException:
+                print(
+                    "Read the file:" +
+                    options.filename +
+                    " Error! or Docker is not running!")
         else:
             run(host, options.image)
     else:
--- push-k8s.sh
+++ push-k8s.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 sudo swapoff -a
-echo "Update System.."
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-echo "Install kubeadm & push images.."
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-sudo kubeadm config images list > image.txt
+sudo kubeadm config images list 2> /dev/null 1> image.txt
 sudo docker run --rm -it \
         -v $(pwd)/image.txt:/image-pull/image.txt \
         -v /var/run/docker.sock:/var/run/docker.sock  \
