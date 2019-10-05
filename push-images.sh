8151a08c01ca840c54ec9550301d6d21217c842e
Funky <mrfunky001@gmail.com>
2019-10-05 20:18:33 +0800

    update aliyun sdk version

--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Build Images..."
-sudo docker login -u $username -p $password registry.cn-hangzhou.aliyuncs.com
 sudo docker build -t registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:latest .
 echo "Push Images..."
 sudo docker push registry.cn-hangzhou.aliyuncs.com/geekcloud/image-pull:latest
